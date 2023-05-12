43816303b74cc90cb2e99b37d9d427d1c90a28d3
nagato-yt <nagatoyutoyuto@gmail.com>
2023-05-12 23:53:29 +0900

    標準入力と標準出力

--- java/test4.java
+++ java/test4.java
@@ -1,6 +1,9 @@
 public class test4 {
-  static public void main(String[] args){
-   System.out.println(123);
-   System.out.println(1234);
+  public static void main(String[] args){
+   int num1, num2;
+   num1 = 5;
+   System.out.println("num1は"+num1+"です");
+   num2 = num1; //num1を代入した。
+   System.out.println("num2は"+num2+"です");
   }
 }
